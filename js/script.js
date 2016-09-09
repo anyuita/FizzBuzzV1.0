93a139e9a8b1605799cc859c5583e7885229a4e8
Anna Nemchuk <anyuita@gmail.com>
2016-09-09 12:41:12 -0700

    Added paragraphs

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 for (var a = 1; a < 101; a++) {
 	if (a % 3 === 0 && a % 5 === 0) {
-		document.write("fizz buzz");
+		document.write("<p>fizz buzz</p>");
 	}
 	else if (a % 3 === 0) {
-		document.write("fizz");
+		document.write("<p>fizz</p>");
 	}
 	else if (a % 5 === 0) {
-		document.write("buzz");
+		document.write("<p>buzz</p>");
 	}
 	else {
-		document.write(a);
+		document.write("<p>" + a + "</p>");
 	}
 }
