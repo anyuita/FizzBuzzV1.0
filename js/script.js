c1ced1d2eee814cd34cd043857e3a1220aa884b3
Anna Nemchuk <anyuita@gmail.com>
2016-09-20 11:32:22 -0700

    Refactored FizzBuzz - added user input & made into function

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
-for (var a = 1; a < 101; a++) {
-	if (a % 3 === 0 && a % 5 === 0) {
-		document.write("<p>fizz buzz</p>");
-	}
-	else if (a % 3 === 0) {
-		document.write("<p>fizz</p>");
-	}
-	else if (a % 5 === 0) {
-		document.write("<p>buzz</p>");
-	}
-	else {
-		document.write("<p>" + a + "</p>");
+var userInput = parseInt(prompt("Please choose a integer"));
+while (isNaN(userInput)) {
+	alert("Please make sure your answer is an integer");
+	var userInput = parseInt(prompt("Please choose a integer"));
+}
+
+countTo100Fizzily(userInput);
+function countTo100Fizzily (valueOfUserNumber) {
+	for (var a = 1; a < valueOfUserNumber; a++) {
+		if (a % 3 === 0 && a % 5 === 0) {
+			document.write("<p>fizz buzz</p>");
+		}
+		else if (a % 3 === 0) {
+			document.write("<p>fizz</p>");
+		}
+		else if (a % 5 === 0) {
+			document.write("<p>buzz</p>");
+		}
+		else {
+			document.write("<p>" + a + "</p>");
+		}
 	}
 }
